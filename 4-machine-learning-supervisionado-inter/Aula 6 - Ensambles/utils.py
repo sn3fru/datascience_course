d03bad3d0cf640b004f3c4a6c23405116113d160
marcos <marcosviniciusenator@gmail.com>
2019-01-25 14:32:48 -0200

    Add module 4 with machine learning II

---
+++ 4-machine-learning-supervisionado-inter/Aula 6 - Ensambles/utils.py
@@ -0,0 +1,365 @@
+import matplotlib.pyplot as plt
+import seaborn
+import pandas as pd
+import numpy as np
+import pickle
+# from mlxtend.plotting import plot_decision_regions
+from itertools import cycle
+from sklearn.datasets import *
+
+
+colors = seaborn.color_palette('Set1')
+colors_str = ','.join(colors.as_hex())
+
+
+def load_cancer():
+	cancer = load_breast_cancer()
+	x = pd.DataFrame(cancer.data, columns=cancer.feature_names)
+	y = pd.Series(cancer.target)
+	return x, y, list(cancer.target_names)
+
+def load_fraud():
+	frauds = pd.read_csv('../data/fraud.csv')
+	y = frauds.is_fraud 
+	x = frauds.drop('is_fraud', axis=1)
+	return x, y
+
+def load_games():
+	df = pd.read_csv('../data/games.csv')
+	y = df.Critic_Score
+	x = df.drop('Critic_Score', axis=1)
+	return x, y
+
+def load_default():
+    df = pd.read_csv('../data/default.csv')
+    y = df.SeriousDlqin2yrs
+    x = df.drop('SeriousDlqin2yrs', axis=1)
+    return x, y
+    
+def load_nonlinear():
+	return load_pickle('../data/non-linear.p')
+
+def load_playing():
+	return load_pickle('../data/playing.p')
+
+def load_regression():
+	return load_pickle('../data/regression.p')
+
+def load_pickle(path):
+	with open(path, 'rb') as fd:
+	    x, y = pickle.load(fd)
+	return x, y
+
+
+datasets = {'non-linear': load_nonlinear,
+			'playing': load_playing,
+			'cancer': load_cancer,
+			'fraud': load_fraud,
+			'regression': load_regression,
+			'games': load_games,
+            'default': load_default}
+
+
+def load_dataset(name):
+	return datasets[name]()
+
+def check_Xy(X, y, y_int=True):
+
+    # check types
+    if not isinstance(X, np.ndarray):
+        raise ValueError('X must be a NumPy array. Found %s' % type(X))
+    if not isinstance(y, np.ndarray):
+        raise ValueError('y must be a NumPy array. Found %s' % type(y))
+
+    if y_int and not np.issubdtype(y.dtype, np.integer):
+        raise ValueError('y must be an integer array. Found %s. '
+                         'Try passing the array as y.astype(np.integer)'
+                         % y.dtype)
+
+    if X.dtype not in (np.float, np.int):
+        raise ValueError('X must be an integer or float array. Found %s.'
+                         % X.dtype)
+
+    # check dim
+    if len(X.shape) != 2:
+        raise ValueError('X must be a 2D array. Found %s' % str(X.shape))
+    if len(y.shape) > 1:
+        raise ValueError('y must be a 1D array. Found %s' % str(y.shape))
+
+    # check other
+    if y.shape[0] != X.shape[0]:
+        raise ValueError('y and X must contain the same number of samples. '
+                         'Got y: %d, X: %d' % (y.shape[0], X.shape[0]))
+
+
+def get_feature_range_mask(X, filler_feature_values=None,
+                           filler_feature_ranges=None):
+    '''Function that constucts a boolean array to get rid of samples
+    in X that are outside the feature range specified by filler_feature_values
+    and filler_feature_ranges'''
+
+    if not isinstance(X, np.ndarray) or not len(X.shape) == 2:
+        raise ValueError('X must be a 2D array')
+    elif filler_feature_values is None:
+        raise ValueError('filler_feature_values must not be None')
+    elif filler_feature_ranges is None:
+        raise ValueError('filler_feature_ranges must not be None')
+
+    mask = np.ones(X.shape[0], dtype=bool)
+    for feature_idx in filler_feature_ranges:
+        feature_value = filler_feature_values[feature_idx]
+        feature_width = filler_feature_ranges[feature_idx]
+        upp_limit = feature_value + feature_width
+        low_limit = feature_value - feature_width
+        feature_mask = (X[:, feature_idx] > low_limit) & \
+                       (X[:, feature_idx] < upp_limit)
+        mask = mask & feature_mask
+
+    return mask
+
+
+def plot_decision_regions(X, y, clf,
+                          feature_index=None,
+                          filler_feature_values=None,
+                          filler_feature_ranges=None,
+                          ax=None,
+                          X_highlight=None,
+                          res=0.02, legend=1,
+                          hide_spines=True,
+                          markers='s^oxv<>',
+                          colors='red,blue,limegreen,gray,cyan'):
+    """Plot decision regions of a classifier.
+    Please note that this functions assumes that class labels are
+    labeled consecutively, e.g,. 0, 1, 2, 3, 4, and 5. If you have class
+    labels with integer labels > 4, you may want to provide additional colors
+    and/or markers as `colors` and `markers` arguments.
+    See http://matplotlib.org/examples/color/named_colors.html for more
+    information.
+    Parameters
+    ----------
+    X : array-like, shape = [n_samples, n_features]
+        Feature Matrix.
+    y : array-like, shape = [n_samples]
+        True class labels.
+    clf : Classifier object.
+        Must have a .predict method.
+    feature_index : array-like (default: (0,) for 1D, (0, 1) otherwise)
+        Feature indices to use for plotting. The first index in
+        `feature_index` will be on the x-axis, the second index will be
+        on the y-axis.
+    filler_feature_values : dict (default: None)
+        Only needed for number features > 2. Dictionary of feature
+        index-value pairs for the features not being plotted.
+    filler_feature_ranges : dict (default: None)
+        Only needed for number features > 2. Dictionary of feature
+        index-value pairs for the features not being plotted. Will use the
+        ranges provided to select training samples for plotting.
+    ax : matplotlib.axes.Axes (default: None)
+        An existing matplotlib Axes. Creates
+        one if ax=None.
+    X_highlight : array-like, shape = [n_samples, n_features] (default: None)
+        An array with data points that are used to highlight samples in `X`.
+    res : float or array-like, shape = (2,) (default: 0.02)
+        Grid width. If float, same resolution is used for both the x- and
+        y-axis. If array-like, the first item is used on the x-axis, the
+        second is used on the y-axis. Lower values increase the resolution but
+        slow down the plotting.
+    hide_spines : bool (default: True)
+        Hide axis spines if True.
+    legend : int (default: 1)
+        Integer to specify the legend location.
+        No legend if legend is 0.
+    markers : str (default 's^oxv<>')
+        Scatterplot markers.
+    colors : str (default 'red,blue,limegreen,gray,cyan')
+        Comma separated list of colors.
+    Returns
+    ---------
+    ax : matplotlib.axes.Axes object
+    """
+
+    check_Xy(X, y, y_int=True)  # Validate X and y arrays
+    dim = X.shape[1]
+
+    if ax is None:
+        ax = plt.gca()
+
+    if isinstance(res, float):
+        xres, yres = res, res
+    else:
+        try:
+            xres, yres = res
+        except ValueError:
+            raise ValueError('Unable to unpack res. Expecting '
+                             'array-like input of length 2.')
+
+    plot_testdata = True
+    if not isinstance(X_highlight, np.ndarray):
+        if X_highlight is not None:
+            raise ValueError('X_highlight must be a NumPy array or None')
+        else:
+            plot_testdata = False
+    elif len(X_highlight.shape) < 2:
+        raise ValueError('X_highlight must be a 2D array')
+
+    if feature_index is not None:
+        # Unpack and validate the feature_index values
+        if dim == 1:
+            raise ValueError(
+                'feature_index requires more than one training feature')
+        try:
+            x_index, y_index = feature_index
+        except ValueError:
+            raise ValueError(
+                'Unable to unpack feature_index. Make sure feature_index '
+                'only has two dimensions.')
+        try:
+            X[:, x_index], X[:, y_index]
+        except IndexError:
+            raise IndexError(
+                'feature_index values out of range. X.shape is {}, but '
+                'feature_index is {}'.format(X.shape, feature_index))
+    else:
+        feature_index = (0, 1)
+        x_index, y_index = feature_index
+
+    # Extra input validation for higher number of training features
+    if dim > 2:
+        if filler_feature_values is None:
+            raise ValueError('Filler values must be provided when '
+                             'X has more than 2 training features.')
+
+        if filler_feature_ranges is not None:
+            if not set(filler_feature_values) == set(filler_feature_ranges):
+                raise ValueError(
+                    'filler_feature_values and filler_feature_ranges must '
+                    'have the same keys')
+
+        # Check that all columns in X are accounted for
+        column_check = np.zeros(dim, dtype=bool)
+        for idx in filler_feature_values:
+            column_check[idx] = True
+        for idx in feature_index:
+            column_check[idx] = True
+        if not all(column_check):
+            missing_cols = np.argwhere(~column_check).flatten()
+            raise ValueError(
+                'Column(s) {} need to be accounted for in either '
+                'feature_index or filler_feature_values'.format(missing_cols))
+
+    marker_gen = cycle(list(markers))
+
+    n_classes = np.unique(y).shape[0]
+    colors = colors.split(',')
+    colors_gen = cycle(colors)
+    colors = [next(colors_gen) for c in range(n_classes)]
+
+    # Get minimum and maximum
+    x_min, x_max = X[:, x_index].min() - 1, X[:, x_index].max() + 1
+    if dim == 1:
+        y_min, y_max = -1, 1
+    else:
+        y_min, y_max = X[:, y_index].min() - 1, X[:, y_index].max() + 1
+
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, xres),
+                         np.arange(y_min, y_max, yres))
+
+    if dim == 1:
+        X_predict = np.array([xx.ravel()]).T
+    else:
+        X_grid = np.array([xx.ravel(), yy.ravel()]).T
+        X_predict = np.zeros((X_grid.shape[0], dim))
+        X_predict[:, x_index] = X_grid[:, 0]
+        X_predict[:, y_index] = X_grid[:, 1]
+        if dim > 2:
+            for feature_idx in filler_feature_values:
+                X_predict[:, feature_idx] = filler_feature_values[feature_idx]
+    Z = clf.predict(X_predict)
+    Z = Z.reshape(xx.shape)
+    # Plot decisoin region
+    ax.contourf(xx, yy, Z,
+                alpha=0.3,
+                colors=colors,
+                levels=np.arange(Z.max() + 2) - 0.5)
+
+    ax.axis(xmin=xx.min(), xmax=xx.max(), y_min=yy.min(), y_max=yy.max())
+
+    # Scatter training data samples
+    for idx, c in enumerate(np.unique(y)):
+        if dim == 1:
+            y_data = [0 for i in X[y == c]]
+            x_data = X[y == c]
+        elif dim == 2:
+            y_data = X[y == c, y_index]
+            x_data = X[y == c, x_index]
+        elif dim > 2 and filler_feature_ranges is not None:
+            class_mask = y == c
+            feature_range_mask = get_feature_range_mask(
+                            X, filler_feature_values=filler_feature_values,
+                            filler_feature_ranges=filler_feature_ranges)
+            y_data = X[class_mask & feature_range_mask, y_index]
+            x_data = X[class_mask & feature_range_mask, x_index]
+        else:
+            continue
+
+        ax.scatter(x=x_data,
+                   y=y_data,
+                   alpha=0.8,
+                   c=colors[idx],
+                   marker=next(marker_gen),
+                   edgecolor='black',
+                   label=c)
+
+    if hide_spines:
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+    ax.yaxis.set_ticks_position('left')
+    ax.xaxis.set_ticks_position('bottom')
+    if dim == 1:
+        ax.axes.get_yaxis().set_ticks([])
+
+    if legend:
+        if dim > 2 and filler_feature_ranges is None:
+            pass
+        else:
+            handles, labels = ax.get_legend_handles_labels()
+            ax.legend(handles, labels,
+                      framealpha=0.3, scatterpoints=1, loc=legend)
+
+    if plot_testdata:
+        if dim == 1:
+            x_data = X_highlight
+            y_data = [0 for i in X_highlight]
+        elif dim == 2:
+            x_data = X_highlight[:, x_index]
+            y_data = X_highlight[:, y_index]
+        else:
+            feature_range_mask = get_feature_range_mask(
+                    X_highlight, filler_feature_values=filler_feature_values,
+                    filler_feature_ranges=filler_feature_ranges)
+            y_data = X_highlight[feature_range_mask, y_index]
+            x_data = X_highlight[feature_range_mask, x_index]
+
+        ax.scatter(x_data,
+                   y_data,
+                   c='',
+                   edgecolor='black',
+                   alpha=1.0,
+                   linewidths=1,
+                   marker='o',
+                   s=80)
+
+    return ax
+
+
+def plot2d(x, y, clf=None):
+	if clf is not None:
+		plot_decision_regions(x, y, 
+                      clf=clf, 
+                      markers='ooooo',
+                      colors=colors_str)
+	else:
+		plt.scatter(x=x[y==0,0], y=x[y==0,1], color=colors[0])
+		plt.scatter(x=x[y==1,0], y=x[y==1,1], color=colors[1])
